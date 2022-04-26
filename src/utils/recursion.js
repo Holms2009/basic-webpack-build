c15de63cf3ad2aa649b0519f196321266b88ccab
Holms2009 <holms200990@gmail.com>
2022-04-26 23:28:46 +0300

    Small function fix

--- src/utils/recursion.js
+++ src/utils/recursion.js
@@ -2,7 +2,7 @@ function recursive(obj) {
   for (let key in obj) {
     if (typeof obj[key] === 'object') recursive(obj[key]);
 
-    if (!obj.length) {
+    if (!obj.length && key.toUpperCase() !== key) {
       obj[key.toUpperCase()] = obj[key];
       delete obj[key];
     }
