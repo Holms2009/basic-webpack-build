17f4114777b6208c71df651d92462c357725ad98
Holms2009 <holms200990@gmail.com>
2022-04-26 22:18:33 +0300

    Added working tip for input

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './styles/main.scss';
 class CityPicker {
   constructor() {
     this.inputField = document.getElementById('city-input');
+    this.tip = document.getElementById('city-tip-list');
     this.fetcher = null;
     this.state = {
       fieldValue: '',
       citiesList: new Set(),
+      pickedCity: null,
     };
     this.handlers = {
-      inputHandler: this.#onInputHandler.bind(this)
+      inputHandler: this.#onInputHandler.bind(this),
+      cityPickHandler: this.#pickCityOnClick.bind(this),
     };
 
     this.#createFetcher();
@@ -26,25 +29,50 @@ class CityPicker {
           if (item.id !== 'Free') this.state.citiesList.add(item.name);
         })
 
-        console.log(this.state.citiesList);
+        this.#updateTip();
       },
       onError: function () {
         console.log('Request timeout!!!');
       },
-      timeoputLimit: 5,
-      callbackName: 'fetcherCallback'
+      timeoutLimit: 4,
+      callbackName: 'fetcherCallback',
     }
 
     this.fetcher = new Fetcher(options);
   }
 
   #onInputHandler(evt) {
+    this.pickedCity = null;
     this.state.fieldValue = evt.currentTarget.value;
     this.inputField.value = this.state.fieldValue;
+    this.#updateTip();
 
-    if (this.state.fieldValue.length > 2) this.fetcher.sendRequest(
-      `https://kladr-api.ru/api.php?query=${this.state.fieldValue}&contentType=city&withParent=1&limit=10`
+    if (this.state.fieldValue.length > 1) this.fetcher.sendRequest(
+      `https://kladr-api.ru/api.php?query=${this.state.fieldValue}&contentType=city&withParent=0&limit=10`
     );
+  }
+
+  #updateTip() {
+    const tipIsNeeded = (!this.pickedCity && this.state.fieldValue.length > 1 && this.state.citiesList.size);
+    this.tip.parentElement.style.display = tipIsNeeded ? 'block' : 'none';
+    this.tip.innerHTML = '';
+
+    if (!tipIsNeeded) return false;
+
+    this.state.citiesList.forEach(city => {
+      const item = document.createElement('li');
+
+      item.classList.add('city-picker__item');
+      item.textContent = city;
+      item.addEventListener('click', this.handlers.cityPickHandler);
+
+      this.tip.appendChild(item);
+    })
+  }
+
+  #pickCityOnClick(evt) {
+    this.state.fieldValue = this.inputField.value = this.pickedCity = evt.currentTarget.textContent;
+    this.#updateTip();
   }
 }
 
